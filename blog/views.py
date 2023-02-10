d52134dc4f069afd83ed2f4df64a536c79a7806e
Lucas Gitirana <gitiranalucas5@gmail.com>
2023-02-10 09:03:00 -0300

    special templates features

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from datetime import date
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
@@ -27,7 +27,7 @@ posts = [
         "slug": "programming-is-fun",
         "image": "coding.jpg",
         "author": "Maximilian",
-        "date": date(2022, 2, 9),
+        "date": date(2022, 3, 9),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -48,7 +48,7 @@ posts = [
         "slug": "into-the-woods",
         "image": "woods.jpg",
         "author": "Maximilian",
-        "date": date(2022, 2, 9),
+        "date": date(2022, 2, 10),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
@@ -67,10 +67,17 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post['date']
+
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
     return render(request, "blog/all-posts.html")
